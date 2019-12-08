05d0b755f849c75c3ef838cef6563a516eeb6126
Kae <gea.24082540@hotmail.com>
2019-12-08 11:11:00 +0700

    Add messge

--- src/lab1/Hello.java
+++ src/lab1/Hello.java
@@ -5,8 +5,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello World,I am Kae");
-
-
+        System.out.println("MIT");
+        System.out.println("Faculty o Management Technology");
 
 
     }//main
